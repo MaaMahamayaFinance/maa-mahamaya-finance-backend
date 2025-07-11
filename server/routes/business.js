299dfd2325e801f22bb2854d17ca24a0029dee83
Nitin Khare <kharenitin777@gmail.com>
2025-07-11 20:16:34 +0530

    updated get kyc routes

--- server/routes/business.js
+++ server/routes/business.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const authenticateUser = require("../middleware/authenticate");
 const { getAllBusinessController,createBusinessIdCardController, getMyBusinessIdCard, createBusinessCertificateController, getMyBusinessCertificate, searchBusinessByUniqueIdController, deleteBusinessController, submitAadhaarPanController } = require("../controller/businessController");
-const { getKYCDetailsController } = require('../controller/aadharPanController');
 
 
 
@@ -14,7 +13,6 @@ router.get("/authorizationcertificate/me", authenticateUser, getMyBusinessCertif
 router.get('/searchbusiness', searchBusinessByUniqueIdController);
 router.delete('/business/:uniqueId', deleteBusinessController);
 router.post('/business/kyc', submitAadhaarPanController);
-router.get('/business/getkyc/:userId', getKYCDetailsController);
 
 
 module.exports = router;
